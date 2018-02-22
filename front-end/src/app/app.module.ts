39616604782e1357e2ff61815417f79425c6cd11
benreckas <benreckas@gmail.com>
2018-02-22 15:06:48 -0800

    Fix broken componet routes in app.modules.ts

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,19 +6,19 @@ import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { SearchResultComponent } from './components/search-result/search-result.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { AboutComponent } from './components/about/about.component';
-import { EducationComponent } from './components/education/education.component';
-import { ExperienceComponent } from './components/experience/experience.component';
-import { AchievementsComponent } from './components/achievements/achievements.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { SearchBarComponent } from './components/search/search-bar/search-bar.component';
+import { SearchResultComponent } from './components/search/search-result/search-result.component';
+import { NavbarComponent } from './components/profile/navbar/navbar.component';
+import { AboutComponent } from './components/profile/about/about.component';
+import { EducationComponent } from './components/profile/education/education.component';
+import { ExperienceComponent } from './components/profile/experience/experience.component';
+import { AchievementsComponent } from './components/profile/achievements/achievements.component';
+import { ProjectsComponent } from './components/profile/projects/projects.component';
+import { SkillsComponent } from './components/profile/skills/skills.component';
+import { LoginComponent } from './components/home/login/login.component';
+import { RegisterComponent } from './components/home/register/register.component';
+import { HeroComponent } from './components/home/hero/hero.component';
+import { FooterComponent } from './components/home/footer/footer.component';
 
 
 @NgModule({
