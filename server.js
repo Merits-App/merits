01f35294d7eb0a570eadf2f5ae898a6386c7fd1e
David Cervantes <pakawaka1@gmail.com>
2018-02-25 06:20:27 -0800

    create initial docker files for front- and back-ends

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+// Get our API routes
+const api = require('./routes/api');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set our api routes
+app.use('/', api);
+
+// Get port from environment and store in Express
+const port = process.env.PORT || '8000';
+app.set('port', port);
+
+// Create HTTP server
+
+const server = http.createServer(app);
+
+// Listen on provided port, on all network interfaces
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
