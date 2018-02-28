2f1cb13c4ba20d0b0bb622ead4787286cd48c12e
benreckas <benreckas@gmail.com>
2018-02-27 16:56:12 -0800

    Static layout of project component.

--- front-end/src/app/components/profile/projects/projects.component.ts
+++ front-end/src/app/components/profile/projects/projects.component.ts
@@ -10,6 +10,22 @@ export class ProjectsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    (function showInfo() {
+      const projects = document.querySelectorAll('.placeholder');
+      for (let i = 0; i < projects.length; i++) {
+        projects[i].addEventListener('mouseover', (e) => {
+          e.srcElement.children[1].classList.add('show');
+          e.srcElement.children[1].classList.remove('hide');
+          console.log('enter', e);
+        });
+        projects[i].addEventListener('mouseleave', (e) => {
+          e.srcElement.children[1].classList.add('hide');
+          e.srcElement.children[1].classList.remove('show');
+          console.log('leave', e);
+        });
+      }
+    }());
   }
 
 }
