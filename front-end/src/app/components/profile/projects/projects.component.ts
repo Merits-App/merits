f70468269318ccca9a8cab5b878800d3f2d4c639
benreckas <benreckas@gmail.com>
2018-02-28 10:35:09 -0800

    Add angular events

--- front-end/src/app/components/profile/projects/projects.component.ts
+++ front-end/src/app/components/profile/projects/projects.component.ts
@@ -11,21 +11,18 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit() {
 
-    (function showInfo() {
-      const projects = document.querySelectorAll('.placeholder');
-      for (let i = 0; i < projects.length; i++) {
-        projects[i].addEventListener('mouseover', (e) => {
-          e.srcElement.children[1].classList.add('show');
-          e.srcElement.children[1].classList.remove('hide');
-          console.log('enter', e);
-        });
-        projects[i].addEventListener('mouseleave', (e) => {
-          e.srcElement.children[1].classList.add('hide');
-          e.srcElement.children[1].classList.remove('show');
-          console.log('leave', e);
-        });
-      }
-    }());
+  }
+
+  show($event) {
+    // e.srcElement.children[1].classList.add('show');
+    // e.srcElement.children[1].classList.remove('hide');
+    console.log('show');
+  }
+
+  hide($event) {
+    // e.srcElement.children[1].classList.add('show');
+    // e.srcElement.children[1].classList.remove('hide');
+    console.log('hide');
   }
 
 }
