5cd9f150c9766624a9456ad6c38dae253ec22bdf
benreckas <benreckas@gmail.com>
2018-03-01 11:58:55 -0800

    Add angular events

--- front-end/src/app/components/profile/projects/projects.component.ts
+++ front-end/src/app/components/profile/projects/projects.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+  // Properties
 
+  // Methods
   constructor() { }
 
   ngOnInit() {
