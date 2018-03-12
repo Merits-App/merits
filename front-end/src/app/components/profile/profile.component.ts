44e46fc837aba0f11c59c490d9482620ae1aae43
benreckas <benreckas@gmail.com>
2018-03-12 08:53:08 -0700

    Fix conflicts after upstream merge

--- front-end/src/app/components/profile/profile.component.ts
+++ front-end/src/app/components/profile/profile.component.ts
@@ -20,25 +20,28 @@ export class ProfileComponent implements OnInit {
     // Check window size on page load
     const screenWidth = window.innerWidth;
     // If window is larger than 1000px then show desktop menu, else show mobile menu
-    screenWidth >= 1000 ? this.menu = true : this.menu = false;
+    screenWidth >= 1000 ? this.showNav() : this.hideNav();
 
   }
 
+  // Check window width on resize. Larger than 1000px show nave, else hide nav
+  toggleNav($event) {
+    $event.target.innerWidth >= 1000 ? this.showNav() : this.hideNav();
+  }
+
+  hideModal() {
+    const popup = document.querySelector('.popup-bg');
+    popup.setAttribute('style', 'display: none');
+  }
+
+  // Display Nav for Desktop
   showNav() {
     this.menu = true;
   }
 
+  // Hide Nav in Hamburger Menu for Mobile
   hideNav() {
     this.menu = false;
   }
 
-  toggleNav($event) {
-    $event.target.innerWidth >= 1000 ? this.menu = true : this.menu = false;
-  }
-  hideModal(){
-    var popup = document.querySelector('.popup-bg');
-    
-    popup.setAttribute("style", "display: none");
-    console.log('works');
-  }
 }
