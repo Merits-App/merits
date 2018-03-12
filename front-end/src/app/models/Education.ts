fea930910214683220a0192120c6ed43f149d981
benreckas <benreckas@gmail.com>
2018-03-12 14:53:38 -0700

    Fix app model syntax

--- front-end/src/app/models/Education.ts
+++ front-end/src/app/models/Education.ts
@@ -1,10 +1,10 @@
 export interface Education {
-  userId: string,
-  edId: string,
-  dateCompletion?: number,
-  current?: boolean,
-  institutionName: string,
-  fieldOfStudy: string,
-  highestDeg?: string,
-  comments?: string
-};
+  userId: string;
+  edId: string;
+  dateCompletion?: number;
+  current?: boolean;
+  institutionName: string;
+  fieldOfStudy: string;
+  highestDeg?: string;
+  comments?: string;
+}
