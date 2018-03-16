4155fabfc1227a028258aee2b1cfa75131f40d91
Jacque <ayojackay@gmail.com>
2018-03-16 14:56:59 -0700

    got register up and running - an issue

--- front-end/src/app/components/home/register/register.component.ts
+++ front-end/src/app/components/home/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
+import { ValidateService } from '../../../services/validate.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,12 +9,51 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  firstName: String;
+  lastName: String;
+  email: String;
+  password: String;
+  city: String;
+  state: String;
+  confirmPassword: String;
+  
+  constructor(
+    private validateService: ValidateService,
+    private authService: AuthService,
+  private router: Router) 
+    {
+
+    }
+
 
   ngOnInit() {
   }
 
-  onRegisterSubmit() {
+  registrationSubmit() {
+    const user = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      password: this.password,
+      city: this.city,
+      state: this.state
+    };
+    
+  if (!this.validateService.validateRegister(user)) {
+    console.log('Please fill in all fields');
   }
 
+  if (!this.validateService.validateEmail(user.email)) {
+    console.log('Please enter a valid email');
+    return false;
+  }
+  
+  this.authService.registerUser(user).subscribe(data => {
+    if(data === true) {
+      console.log('You are now registered!');
+      this.router.navigate(['/app-profile']);
+    } else {
+        console.log('Something went wrong!');
+      }
+  });
 }
-
